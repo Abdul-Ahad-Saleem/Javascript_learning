f53e19196d451e5ca794d11dbb22d03ecab3a002
Abdul-Ahad-Saleem <130921042+Abdul-Ahad-Saleem@users.noreply.github.com>
2024-06-27 19:34:07 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,91 @@
+// Premitive DataTypes
+
+// Which can be callable by their values
+
+// 1.String Datatype
+let str  = "Ahmad"; 
+console.log("str : " + str);
+console.log(typeof str);
+
+console.log('-------------------------------');
+// 2.Number Datatype is used to represent int as well as float numbers 
+
+// Number dataType for Integer numbers 
+let num = 10345;
+console.log(num);
+console.log(typeof num);
+
+console.log('-------------------------------');
+
+// Number dataType for Float numbers
+let fl = 103.14;
+console.log(fl);
+console.log(typeof fl);
+
+console.log('-------------------------------');
+
+// 3.Boolean DataType
+
+let shift = true;
+console.log(shift);
+console.log(typeof shift)
+
+console.log('-------------------------------');
+
+// 4.Null Datatype
+let n = null;
+console.log(n);
+console.log(typeof n); // it's type is object
+
+console.log('-------------------------------');
+
+// 5.BigInt DataType
+
+// You have to add "n" at the end of the number to kmake it bigInt
+let biggerNum = 12222333333333356343464634634n;
+console.log(biggerNum);
+console.log(typeof biggerNum);
+
+console.log('-------------------------------');
+
+// 6.Undefined
+
+let undef ; 
+console.log(undef);
+console.log(typeof undef);
+
+console.log('-------------------------------');
+
+// Non Premitive DataTypes
+
+// 1.Array of List
+let arr = ['Ali','Ahmad','Amjad'];
+console.log(arr);
+console.log(typeof arr);
+
+console.log('-------------------------------');
+
+// 2. Array of Dictionary
+let keyValue = {
+    1 : 'Ali',
+    2 : "Ahmad",
+    3 : "Amjad", 
+}
+console.log(keyValue);
+console.log(typeof (keyAVlue));
+
+console.log('-------------------------------');
+
+// 3. Function
+let fun = function nami(){
+    return 2+2;
+}
+
+console.log(fun);
+console.log(typeof (fun));
+
+
+
+
+
+
